9db2558fb13d4a69d6bfc0f351bf098b31537555
1nside1 <kas.lietuvis123@gmail.com>
2026-09-23 16:35:40 +0300

    Pridėtas pirmas mygtukas ir issokantis pasirinktas tekstas

--- app/src/main/java/com/example/miptpraktika1soft/MainActivity.java
+++ app/src/main/java/com/example/miptpraktika1soft/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.miptpraktika1soft;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+// Susiejame Java kintamuosius su XML dizaino elementais pagal jų ID
+        TextView manoTekstas = findViewById(R.id.textView1);
+        Button mygtukas1 = findViewById(R.id.button1);
+
+        // Nustatome, kas įvyks paspaudus mygtuką
+        mygtukas1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                manoTekstas.setText("ISSOKANTIS TEKSTAS!");
+            }
         });
     }
 }
